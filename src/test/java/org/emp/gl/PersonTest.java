8dd997737d09fcf5a83819fc3d672041d14b5202
oussama <oussama.rebidj@gmail.com>
2025-03-24 12:47:36 +0100

    exo1 updated

--- src/test/java/org/emp/gl/PersonTest.java
+++ src/test/java/org/emp/gl/PersonTest.java
@@ -6,22 +6,22 @@ class PersonTest {
 
     @Test
     void getFullNameShouldReturnFirstNameAndLastName() {
-        Person person = new Person("John", "Doe", 25);
-        assertEquals("John Doe", person.getFullName());
+        Person person = new Person("oussama", "rebidj", 25);
+        assertEquals("oussama rebidj", person.getFullName());
     }
 
     @Test
     void isAdultShouldReturnTrueForAge18OrMore() {
-        Person adult = new Person("Alice", "Smith", 18);
+        Person adult = new Person("fateh", "youcef", 18);
         assertTrue(adult.isAdult());
 
-        Person olderAdult = new Person("Bob", "Brown", 30);
+        Person olderAdult = new Person("adem", "raggam", 30);
         assertTrue(olderAdult.isAdult());
     }
 
     @Test
     void isAdultShouldReturnFalseForAgeLessThan18() {
-        Person minor = new Person("Charlie", "Young", 17);
+        Person minor = new Person("bouchmal", "lakhder", 17);
         assertFalse(minor.isAdult());
 
 
