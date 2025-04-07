a5867ddf4b601b2d998a9f1ef01b7202f5c815a0
oussama <oussama.rebidj@gmail.com>
2025-04-07 13:32:34 +0100

    palindrome

---
+++ src/test/java/org/emp/tp1/LineCoverageTest/Palindrome_CorrectionTest.java
@@ -0,0 +1,36 @@
+package org.emp.tp1.LineCoverageTest;
+
+import org.emp.tp1.Palindrome;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Palindrome_CorrectionTest {
+
+    @Test
+    void testPalindromeSimple() {
+        Assertions.assertTrue(Palindrome.isPalindrome("kayak"));
+    }
+
+    @Test
+    void testPalindromeWithSpacesAndCase() {
+        assertTrue(Palindrome.isPalindrome("Esope reste ici et se repose"));
+    }
+
+    @Test
+    void testNotPalindrome() {
+        assertFalse(Palindrome.isPalindrome("bonjour"));
+    }
+
+    @Test
+    void testEmptyString() {
+        assertTrue(Palindrome.isPalindrome(""));
+    }
+
+    @Test
+    void testNullInput() {
+        assertThrows(NullPointerException.class, () -> Palindrome.isPalindrome(null));
+    }
+
+}
